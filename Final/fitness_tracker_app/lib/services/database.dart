389d0f94f10e904842a1df17d46694d311311455
nicolepav <nicolepav2.0@gmail.com>
2022-06-01 22:18:29 -0700

    firestore

--- Final/fitness_tracker_app/lib/services/database.dart
+++ Final/fitness_tracker_app/lib/services/database.dart
@@ -1,2 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+// https://www.youtube.com/watch?v=mtNA1neFNVo
+class DatabaseService {
+
+  // collection reference
+
+  final CollectionReference exerciseCollection = FirebaseFirestore.instance.collection('exercises');
+
+
+
+}
